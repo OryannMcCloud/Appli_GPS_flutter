a6c169e4de85f9a8dcf65214c9f1ecad0c2e984c
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-04-21 11:11:44 +0200

    map fonctionne avec s10

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(48.8566, 2.3522), // Paris coordinates
-          zoom: 15.0, // zoom level
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
-          ),
-        ],
-      ),
+    return FutureBuilder(
+      future: Geolocator.getCurrentPosition(),
+      builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+        if (snapshot.hasData) {
+          final position = snapshot.data!;
+          return FlutterMap(
+            options: MapOptions(
+              center: LatLng(position.latitude, position.longitude),
+              zoom: 13.0,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: LatLng(position.latitude, position.longitude),
+                    builder: (ctx) => Icon(
+                      Icons.location_on,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
