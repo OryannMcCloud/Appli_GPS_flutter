55d3da29aaa35a368cfae57d861a60aed7b964a0
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-04-21 13:43:04 +0200

    switch button added

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -11,28 +12,64 @@ class HomeScreen extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
         if (snapshot.hasData) {
           final position = snapshot.data!;
-          return FlutterMap(
-            options: MapOptions(
-              center: LatLng(position.latitude, position.longitude),
-              zoom: 13.0,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
-              ),
-              MarkerLayerOptions(
-                markers: [
-                  Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: LatLng(position.latitude, position.longitude),
-                    builder: (ctx) => Icon(
-                      Icons.location_on,
-                      color: Colors.purple,
-                    ),
+          return Stack(
+            children: [
+              FlutterMap(
+                options: MapOptions(
+                  center: LatLng(position.latitude, position.longitude),
+                  zoom: 13.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c'],
+                  ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        width: 80.0,
+                        height: 80.0,
+                        point: LatLng(position.latitude, position.longitude),
+                        builder: (ctx) => Icon(
+                          Icons.location_on,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
+              ),
+              Positioned(
+                left: 20.0,
+                bottom: 20.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: LiteRollingSwitch(
+                    value: true,
+                    textOn: 'Dark',
+                    textOff: 'Light',
+                    colorOn: Colors.purple,
+                    colorOff: Color.fromARGB(255, 167, 159, 159),
+                    iconOn: Icons.check,
+                    iconOff: Icons.power_settings_new,
+                    onChanged: (bool state) {
+                      print('Current State of SWITCH IS: $state');
+                    },
+                    onDoubleTap: (bool state) {
+                      print('Current State of SWITCH IS: $state');
+                    },
+                    onSwipe: (bool state) {
+                      print('Current State of SWITCH IS: $state');
+                    },
+                    onTap: (bool state) {
+                      print('Current State of SWITCH IS: $state');
+                    },
+
+                  ),
+                ),
               ),
             ],
           );
