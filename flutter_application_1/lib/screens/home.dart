94316a30a9b2ac5f3300a016efddf90facf03ab1
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-04-21 10:24:55 +0200

    la carte fonctionne sur emulateur

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Welcome to the home screen!'),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(48.8566, 2.3522), // Paris coordinates
+          zoom: 15.0, // zoom level
+        ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+        ],
       ),
     );
   }
