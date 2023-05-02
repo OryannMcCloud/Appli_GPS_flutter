a40414f5bf75080a7dc27733221dad24e9810191
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-05-02 15:08:35 +0200

    je teste des trucs tkt

--- flutter_application_1/lib/screens/map_display.dart
+++ flutter_application_1/lib/screens/map_display.dart
@@ -3,33 +3,67 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
 
+
 class MapDisplay extends StatefulWidget {
   @override
   _MapDisplayState createState() => _MapDisplayState();
 }
 
 class _MapDisplayState extends State<MapDisplay> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Map Display'),
+        title: const Text('Map Display'),
       ),
       body: FlutterMap(
         options: MapOptions(
           center: LatLng(51.5, -0.09),
           zoom: 13.0,
         ),
-        layers: [
-          TileLayer(
-            options: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            stream: ['a', 'b', 'c'],
-            mapState: MapState(),
-
-          ),
-        ]
+        layers: [],
       ),
     );
   }
 }
+
+class DownloadedMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 13.0,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c'],
+          tileProvider: CachedNetworkTileProvider(),
+        ),
+      ],
+    );
+  }
+}
+
+
+class Cursor extends StatefulWidget {
+  @override
+  _CursorState createState() => _CursorState();
+}
+
+class _CursorState extends State<Cursor> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+
+  }
+} 
+
+
