5b57ee0614ba388264ca1af07a96e334e591a801
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-05-04 08:02:14 +0200

    creation de la fonction nmea

--- flutter_application_1/lib/screens/map_display.dart
+++ flutter_application_1/lib/screens/map_display.dart
@@ -1,69 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:vector_map_tiles/vector_map_tiles.dart';
+import 'package:vector_tile_renderer/vector_tile_renderer.dart' hide TileLayer;
+// ignore: uri_does_not_exist
+import 'api_key.dart';
 
-
-class MapDisplay extends StatefulWidget {
-  @override
-  _MapDisplayState createState() => _MapDisplayState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MapDisplayState extends State<MapDisplay> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map Display'),
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(51.5, -0.09),
-          zoom: 13.0,
-        ),
-        layers: [],
-      ),
+    return MaterialApp(
+      title: 'vector_map_tiles Example',
+      theme: ThemeData.light(),
+      home: const MyHomePage(title: 'vector_map_tiles Example'),
     );
   }
 }
 
-class DownloadedMap extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          subdomains: ['a', 'b', 'c'],
-          tileProvider: CachedNetworkTileProvider(),
-        ),
-      ],
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-
-class Cursor extends StatefulWidget {
-  @override
-  _CursorState createState() => _CursorState();
-}
-
-class _CursorState extends State<Cursor> {
+class _MyHomePageState extends State<MyHomePage> {
+  final MapController _controller = MapController();
+  Style? _style;
+  Object? _error;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _initStyle();
   }
 
-  @override 
+  void _initStyle() async {
+    try {
+      _style = await _readStyle();
+    } catch (e, stack) {
+      // ignore: avoid_print
+      print(e);
+      // ignore: avoid_print
+      print(stack);
+      _error = e;
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-
+    final children = <Widget>[];
+    if (_error != null) {
+      children.add(Expanded(child: Text(_error!.toString())));
+    } else if (_style == null) {
+      children.add(const Center(child: CircularProgressIndicator()));
+    } else {
+      children.add(Flexible(child: _map(_style!)));
+      children.add(Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [_statusText()]));
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: children)));
   }
-} 
 
+// alternates:
+//   Mapbox - mapbox://styles/mapbox/streets-v12?access_token={key}
+//   Maptiler - https://api.maptiler.com/maps/outdoor/style.json?key={key}
+//   Stadia Maps - https://tiles.stadiamaps.com/styles/outdoors.json?api_key={key}
+  Future<Style> _readStyle() => StyleReader(
+          uri:
+              'https://tiles.stadiamaps.com/styles/outdoors.json?api_key={key}',
+          // ignore: undefined_identifier
+          apiKey: stadiaMapsApiKey,
+          logger: const Logger.console())
+      .read();
 
+  Widget _map(Style style) => FlutterMap(
+        mapController: _controller,
+        options: MapOptions(
+            center: style.center ?? LatLng(49.246292, -123.116226),
+            zoom: style.zoom ?? 10,
+            maxZoom: 22,
+            interactiveFlags: InteractiveFlag.drag |
+                InteractiveFlag.flingAnimation |
+                InteractiveFlag.pinchMove |
+                InteractiveFlag.pinchZoom |
+                InteractiveFlag.doubleTapZoom),
+        children: [
+          VectorTileLayer(
+              tileProviders: style.providers,
+              theme: style.theme,
+              maximumZoom: 22,
+              // tileOffset: TileOffset.mapbox,
+              layerMode: VectorTileLayerMode.raster)
+        ],
+      );
+
+  Widget _statusText() => Padding(
+      padding: const EdgeInsets.only(top: 8, bottom: 8),
+      child: StreamBuilder(
+          stream: _controller.mapEventStream,
+          builder: (context, snapshot) {
+            return Text(
+                'Zoom: ${_controller.zoom.toStringAsFixed(2)} Center: ${_controller.center.latitude.toStringAsFixed(4)},${_controller.center.longitude.toStringAsFixed(4)}');
+          }));
+}
