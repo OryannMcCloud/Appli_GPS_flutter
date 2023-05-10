ea3a1c221a2ab6ef7cffc162b62740d92d4c58a4
Oryann <99395726+OryannMcCloud@users.noreply.github.com>
2023-05-10 16:32:04 +0200

    start over

--- flutter_application_1/lib/screens/login_page.dart
+++ flutter_application_1/lib/screens/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'map_display.dart';
 
 
 class LoginPage extends StatelessWidget {
@@ -54,7 +53,7 @@ class MyCustomForm extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => MapDisplay()),
+                MaterialPageRoute(builder: (context) => HomeScreen()),
               );
             },
             child: const Text('Submit'),
