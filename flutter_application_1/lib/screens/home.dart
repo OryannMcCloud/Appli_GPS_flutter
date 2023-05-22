26623d1eaf878e03a812ec6960f62f5591e48bfa
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-05-22 11:06:28 +0200

    splash screen

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -2,86 +2,37 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 import 'package:mapbox_flutter/mapbox_flutter.dart';
 
-class HomeScreen extends StatefulWidget { //widget pouvant changer d'état (GPS qui change de position)
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
+class _HomeScreenState extends StatelessWidget {
+  //FIXME: Add your Mapbox access token here
+  static const String ACCESS_TOKEN = "YOUR_TOKEN_HERE";
 
-class _HomeScreenState extends State<HomeScreen> {
-  late LatLng _currentPosition; //probleme de l'affichage de la position actuelle
-
-  @override
-  void initState() { //starting state 
-    super.initState();
-    Geolocator.getPositionStream().listen((Position position) { //listen to the position
-      setState(() { //set the state
-        _currentPosition = LatLng(position.latitude, position.longitude); //set the position
-      });
-    });
+  void _pushPage(BuildContext context, HomeScreen page) async {
+    if (!kIsWeb) {
+      final location = Location();
+      final hasPermissions = await location.hasPermission();
+      if (hasPermissions != PermissionStatus.granted) {
+        await location.requestPermission();
+      }
+    }
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: Text(page.title)),
+              body: page,
+            )));
   }
 
   @override
-  Widget build(BuildContext context) { //build the widget
-    return Stack( //stack the widget
-      children: [ //list of widget
-        FlutterMap( //map 
-          options: MapOptions(  //map options
-            center: _currentPosition,   //center of the map
-            zoom: 16.0, //zoom of the map
-          ),
-          layers: [ //layers of the map
-            TileLayerOptions( //tile layer
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", //url of the tile
-              subdomains: ['a', 'b', 'c'], //subdomains of the tile
-              tileProvider: NonCachingNetworkTileProvider(), //tile provider
-            ),
-            MarkerLayerOptions( //marker of position layer (icon)
-              markers: [ //list of markers
-                Marker(
-                  point: _currentPosition, //position of the marker
-                  builder: (ctx) => Image.asset( //replace the marker by an image
-                    'assets/arrow.png', //non absolute path of the image from 
-                    width: 50.0, //choose the size of the image
-                    height: 50.0,
-                  ),
-                  anchorPos: AnchorPos.align(AnchorAlign.center), //anchor to choose the image alignment with your position
-                ),
-              ],
-            ),
-          ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MapboxFlutterMaps examples')),
+      body: ListView.builder(
+        itemCount: _allPages.length,
+        itemBuilder: (_, int index) => ListTile(
+          leading: _allPages[index].leading,
+          title: Text(_allPages[index].title),
+          onTap: () => _pushPage(context, _allPages[index]),
         ),
-        Positioned(
-          left: 20.0, //position of the switch
-          bottom: 20.0,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0), //border radius of the switch
-            ),
-            child: LiteRollingSwitch(
-              value: true,
-              textOn: 'Dark',
-              textOff: 'Light',
-              colorOn: Colors.purple,
-              colorOff: Color.fromARGB(255, 167, 159, 159),
-              iconOn: Icons.check,
-              iconOff: Icons.power_settings_new,
-              onChanged: (bool state) {
-                print('Current State of SWITCH IS: $state');
-              },
-              onDoubleTap: (bool state) {
-                print('Current State of SWITCH IS: $state');
-              },
-              onSwipe: (bool state) {
-                print('Current State of SWITCH IS: $state');
-              },
-              onTap: (bool state) {
-                print('Current State of SWITCH IS: $state');
-              },
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
--- flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import url_launcher_macos
+import location
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
