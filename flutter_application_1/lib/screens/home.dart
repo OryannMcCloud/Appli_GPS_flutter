b5cdef0f75ce0ccab589c13a233172d6b64f734b
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-05-23 15:13:34 +0200

    main done

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -7,21 +7,6 @@ class _HomeScreenState extends StatelessWidget {
   //FIXME: Add your Mapbox access token here
   static const String ACCESS_TOKEN = "sk.eyJ1IjoiY3Jpc2FydGVjaCIsImEiOiJjbGh5Z2FzZGwwbXM0M2VvZXNwdjkyaHA4In0.vLuglOobIZtS2Vf73fUqzw";
 
-  void _pushPage(BuildContext context, HomeScreen page) async {
-    if (!kIsWeb) {
-      final location = Location();
-      final hasPermissions = await location.hasPermission();
-      if (hasPermissions != PermissionStatus.granted) {
-        await location.requestPermission();
-      }
-    }
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (_) => Scaffold(
-              appBar: AppBar(title: Text(page.title)),
-              body: page,
-            )));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- flutter_application_1/lib/ui/splash.dart
+++ flutter_application_1/lib/ui/splash.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:flutter_application_1/screens/home.dart';
 
 class Splash extends StatefulWidget {
@@ -10,24 +12,32 @@ class Splash extends StatefulWidget {
 
 
 class _SplashState extends State<Splash> {
+
   @override
-  void initState() {
+  void InitState() {
     super.initState();
-    _navigatetohome();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Home()));
+    }
   }
-
-  _navigatetohome()async{
-    await Future.delayed(Duration(milliseconds: 4000), () {});
-    Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => HomeScreen(
-        title: 'MapboxFlutterMaps examples')));
-  }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/logo.png', height: 130,),
+          const SizedBox(height: 20,),
+          if(Platform.isIOS)
+            const CupertinoActivityIndicator(
+              radius: 15,
+            )
+          else
+            const CircularProgressIndicator(
+              color: Colors.white,
+            )
+        ],
       ),
     );
   }
