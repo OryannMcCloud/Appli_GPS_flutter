a002f288350df177b87518e341ad4867a7fe39e7
OryannMcCloud <orianne.prochaska@etu.univ-tlse2.fr>
2023-06-09 10:46:09 +0200

    on va retenter

--- flutter_application_1/lib/screens/home.dart
+++ flutter_application_1/lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:latlong2/latlong.dart';
